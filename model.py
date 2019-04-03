027036dac20f7ad6e2216f25d80d82697470d07f
velsarav <velsarav@gmail.com>
2019-04-03 22:02:53 +0000

    Basic lenet implementation

--- model.py
+++ model.py
@@ -12,32 +12,33 @@ with open('./data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-# 	for i in range(3):
-    source_path = line[0]
-    tokens = source_path.split('/')
-    filename = tokens[-1]
-    local_path = "./data/IMG/" + filename
-    image = cv2.imread(local_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-# 	measurements.append(measurement+correction)
-# 	measurements.append(measurement-correction)
+    for i in range(3):
+        source_path = line[i]
+        tokens = source_path.split('/')
+        filename = tokens[-1]
+        local_path = "./data/IMG/" + filename
+        image = cv2.imread(local_path)
+        images.append(image)
+correction = 0.2
+measurement = float(line[3])
+measurements.append(measurement)
+measurements.append(measurement+correction)
+measurements.append(measurement-correction)
 
 
-# augemented_images = []
-# augemented_measurements = []
-# for image, measurement in zip(images, measurements):
-# 	augemented_images.append(image)
-# 	augemented_measurements.append(measurement)
-# 	flipped_image = cv2.flip(image,3)
-# 	flipped_measurement = float(measurement) * -1.0
-# 	augemented_images.append(flipped_image)
-# 	augemented_measurements.append(flipped_measurement)
+augemented_images = []
+augemented_measurements = []
+for image, measurement in zip(images, measurements):
+	augemented_images.append(image)
+	augemented_measurements.append(measurement)
+	flipped_image = cv2.flip(image,3)
+	flipped_measurement = float(measurement) * -1.0
+	augemented_images.append(flipped_image)
+	augemented_measurements.append(flipped_measurement)
 
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augemented_images)
+y_train = np.array(augemented_measurements)
 
 import keras
 from keras.models import Sequential
@@ -47,24 +48,24 @@ from keras.layers.pooling import MaxPooling2D
 
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
-model.add(Dense(1))
-# model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-# model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(MaxPooling2D())
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(MaxPooling2D())
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
 # model.add(Dropout(0.8))
-# model.add(Flatten())
-# model.add(Dense(100))
-# model.add(Dense(50))
-# model.add(Dense(10))
-# model.add(Dense(1))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
 
