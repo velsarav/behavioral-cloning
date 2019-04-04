f85822f4eca0e256c22952cd24b79fdf7e1f2a6b
velsarav <velsarav@gmail.com>
2019-04-04 22:54:53 +0000

    two functions

--- model.py
+++ model.py
@@ -5,14 +5,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-import keras
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda,Dropout
-from keras.layers.convolutional import Convolution2D, Cropping2D
-from keras.layers.pooling import MaxPooling2D
-from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
+# import keras
+# from keras.models import Sequential
+# from keras.layers import Flatten, Dense, Lambda,Dropout
+# from keras.layers.convolutional import Convolution2D, Cropping2D
+# from keras.layers.pooling import MaxPooling2D
+# from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
 
-from keras.models import Model
+# from keras.models import Model
 # %matplotlib inline
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -78,6 +78,48 @@ def img_brighten(image):
 def bias(n):
     return 1. / (n+1.)
 
+def select_image(batch_number, batch_sample, path):
+    img_choice = np.random.randint(3)
+    angle =float(batch_sample[3])
+    img_name = []
+    if angle + bias(batch_number) < np.random.uniform():
+        if img_choice == 0:        
+            img_name = path + batch_sample[1].split('/')[-1]
+            if abs(angle)  > 1:
+                angle += 0.25
+            else:
+                angle += 0.18
+        elif img_choice == 1:
+            img_name = path + batch_sample[0].split('/')[-1]
+        else:
+            img_name = path + batch_sample[2].split('/')[-1]
+            if abs(angle)  > 1:
+                angle -= 0.25
+            else:
+                angle -= 0.18
+    return img_name, angle
+
+def adjust_steering(image, angle, images, angles):
+    if np.random.randint(10) == 0:
+        images.append(image)
+        angles.append(angle)
+    if angle!=0.18 and angle!=-0.18 and angle!=0:
+        if np.random.randint(3) == 0:
+            image_new = np.fliplr(image)
+            angle_new = -angle
+            images.append(image_new)
+            angles.append(angle_new)
+        if np.random.randint(3) == 1 or 2:
+            image_new = img_brighten(image)
+            images.append(image_new)
+            angles.append(angle)
+        if np.random.randint(3) == 2:
+            image_new = np.fliplr(image)
+            angle_new = -angle
+            images.append(image_new)
+            angles.append(angle_new)
+    return images, angles
+
 
 def generator(lines_path, img_path, batch_size = 32):
     path = img_path
@@ -90,43 +132,12 @@ def generator(lines_path, img_path, batch_size = 32):
             batch_samples = lines[offset:offset+batch_size]
             images = []
             angles = []
+            img_name = []
             for batch_sample in batch_samples:
-                img_choice = np.random.randint(3)
-                angle =float(batch_sample[3])
-                if angle + bias(batch_number) < np.random.uniform():
-                    if img_choice == 0:
-                        name = path + batch_sample[1].split('/')[-1]
-                        if abs(angle)  > 1:
-                            angle += 0.25
-                        else:
-                            angle += 0.18
-                    elif img_choice == 1:
-                        name = path + batch_sample[0].split('/')[-1]
-                    else:
-                        name = path + batch_sample[2].split('/')[-1]
-                        if abs(angle)  > 1:
-                            angle -= 0.25
-                        else:
-                            angle -= 0.18
-                    image = cv2.imread(name)
-                    if np.random.randint(10) == 0:
-                        images.append(image)
-                        angles.append(angle)
-                    if angle!=0.18 and angle!=-0.18 and angle!=0:
-                        if np.random.randint(3) == 0:
-                            image_new = np.fliplr(image)
-                            angle_new = -angle
-                            images.append(image_new)
-                            angles.append(angle_new)
-                        if np.random.randint(3) == 1 or 2:
-                            image_new = img_brighten(image)
-                            images.append(image_new)
-                            angles.append(angle)
-                            if np.random.randint(3) == 2:
-                                image_new = np.fliplr(image)
-                                angle_new = -angle
-                                images.append(image_new)
-                                angles.append(angle_new)
+                img_name, angle = select_image(batch_number, batch_sample, path)
+                print(img_name)
+                image = cv2.imread(img_name)
+                images, angles = adjust_steering(image, angle, images, angles)
                 batch_number += 1
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -136,28 +147,28 @@ train_generator = generator(train_data, img_path)
 validate_generator = generator(validation_data, img_path)
 
 
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(MaxPooling2D())
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(MaxPooling2D())
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
-model.add(Dropout(0.8))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
+# model = Sequential()
+# model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+# model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+# # model.add(MaxPooling2D())
+# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+# # model.add(MaxPooling2D())
+# model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(Dropout(0.8))
+# model.add(Flatten())
+# model.add(Dense(100))
+# model.add(Dense(50))
+# model.add(Dense(10))
+# model.add(Dense(1))
 
-model.compile(optimizer='adam', loss='mse')
+# model.compile(optimizer='adam', loss='mse')
 
-file = 'model_gen.h5'
-earlystopper = EarlyStopping(patience=5, verbose = 1)
-checkpointer = ModelCheckpoint(file, monitor='val_loss', verbose = 1, save_best_only=True)
-model.fit_generator(train_generator, steps_per_epoch= len(train_data), validation_data = validate_generator,
-                        validation_steps=len(validation_data), epochs = 1)
-model.save(file)
+# file = 'model_gen.h5'
+# earlystopper = EarlyStopping(patience=5, verbose = 1)
+# checkpointer = ModelCheckpoint(file, monitor='val_loss', verbose = 1, save_best_only=True)
+# model.fit_generator(train_generator, steps_per_epoch= len(train_data), validation_data = validate_generator,
+#                         validation_steps=len(validation_data), epochs = 1)
+# model.save(file)
