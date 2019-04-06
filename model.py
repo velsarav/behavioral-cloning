4187182c15f322aa6bc4d83f8da193fd44b6fdf1
velsarav <velsarav@gmail.com>
2019-04-06 01:01:25 +0000

    basic cnn working

--- model.py
+++ model.py
@@ -5,14 +5,17 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-# import keras
-# from keras.models import Sequential
-# from keras.layers import Flatten, Dense, Lambda,Dropout
-# from keras.layers.convolutional import Convolution2D, Cropping2D
-# from keras.layers.pooling import MaxPooling2D
-# from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
+import keras
+from keras.layers import Input
+from keras.models import Sequential
+from keras.layers.pooling import MaxPooling2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.utils import plot_model
+from keras.optimizers import Adam
 
-# from keras.models import Model
+from keras.models import Model
+from keras.layers import Cropping2D,  Conv2D, Convolution2D, MaxPool2D, Lambda, Flatten, Dense, Dropout, concatenate, ELU, BatchNormalization
+from keras.applications.vgg16 import VGG16
 # %matplotlib inline
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -65,10 +68,10 @@ X_valid, y_valid = read_img_data(validation_data, img_path)
 #Check the data sample
 assert len(X_train) == len(y_train), "X_train {} and y_train {} are not equal".format(len(X_train), len(y_train))
 assert len(X_valid) == len(y_valid), "X_valid {} and y_valid {} are not equal".format(len(X_valid), len(y_valid))
-print('Total Train samples: {}\n Total valid samples: {}'. format(len(X_train), len(X_valid)))  
+print('Total Train samples: {}\nTotal valid samples: {}'. format(len(X_train), len(X_valid)))  
 
 #Data augementation
-def img_brighten(image):
+def img_brighteness(image):
     hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     bias = 0.25
     img_brightness = bias + np.random.uniform()
@@ -78,26 +81,20 @@ def img_brighten(image):
 def bias(n):
     return 1. / (n+1.)
 
-def select_image(batch_number, batch_sample, path):
-    img_choice = np.random.randint(3)
-    angle =float(batch_sample[3])
-    img_name = []
-    if angle + bias(batch_number) < np.random.uniform():
-        if img_choice == 0:        
-            img_name = path + batch_sample[1].split('/')[-1]
-            if abs(angle)  > 1:
-                angle += 0.25
-            else:
-                angle += 0.18
-        elif img_choice == 1:
-            img_name = path + batch_sample[0].split('/')[-1]
+def position_based_angle(angle, angle_position):
+    if angle_position == "positive":
+        if abs(angle)  > 1:
+            angle += 0.25
         else:
-            img_name = path + batch_sample[2].split('/')[-1]
-            if abs(angle)  > 1:
-                angle -= 0.25
-            else:
-                angle -= 0.18
-    return img_name, angle
+            angle += 0.18
+    else:
+        if abs(angle)  > 1:
+            angle -= 0.25
+        else:
+            angle -= 0.18
+    return angle
+        
+  
 
 def adjust_steering(image, angle, images, angles):
     if np.random.randint(10) == 0:
@@ -110,7 +107,7 @@ def adjust_steering(image, angle, images, angles):
             images.append(image_new)
             angles.append(angle_new)
         if np.random.randint(3) == 1 or 2:
-            image_new = img_brighten(image)
+            image_new = img_brighteness(image)
             images.append(image_new)
             angles.append(angle)
         if np.random.randint(3) == 2:
@@ -126,18 +123,31 @@ def generator(lines_path, img_path, batch_size = 32):
     lines = lines_path
     sum_lines = len(lines)
     batch_number = 1
+    angle_position = "positive"
     while 1:
         shuffle(lines)
         for offset  in range(0, sum_lines, batch_size):
             batch_samples = lines[offset:offset+batch_size]
             images = []
             angles = []
-            img_name = []
             for batch_sample in batch_samples:
-                img_name, angle = select_image(batch_number, batch_sample, path)
-                print(img_name)
-                image = cv2.imread(img_name)
-                images, angles = adjust_steering(image, angle, images, angles)
+                img_choice = np.random.randint(3)
+                angle =float(batch_sample[3])
+                if angle + bias(batch_number) < np.random.uniform():
+                     if img_choice == 0:
+                        img_name = path + batch_sample[1].split('/')[-1]
+                        angle_position = "positive"
+                        augementation_angle = position_based_angle(angle, angle_position)
+                     elif img_choice == 1:
+                        img_name = path + batch_sample[0].split('/')[-1]
+                        augementation_angle = angle
+                     else:
+                        img_name = path + batch_sample[2].split('/')[-1]
+                        angle_position = "negative"
+                        augementation_angle = position_based_angle(angle, angle_position)
+                        
+                     image = cv2.imread(img_name)
+                     images, angles = adjust_steering(image, augementation_angle, images, angles)
                 batch_number += 1
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -146,29 +156,109 @@ def generator(lines_path, img_path, batch_size = 32):
 train_generator = generator(train_data, img_path)
 validate_generator = generator(validation_data, img_path)
 
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(MaxPooling2D())
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(MaxPooling2D())
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Dropout(0.8))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
 
-# model = Sequential()
-# model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-# model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-# # model.add(MaxPooling2D())
-# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-# # model.add(MaxPooling2D())
-# model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Dropout(0.8))
-# model.add(Flatten())
-# model.add(Dense(100))
-# model.add(Dense(50))
-# model.add(Dense(10))
-# model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
 
-# model.compile(optimizer='adam', loss='mse')
+file = 'model_new.h5'
+earlystopper = EarlyStopping(patience=5, verbose = 1)
+checkpointer = ModelCheckpoint(file, monitor='val_loss', verbose = 1, save_best_only=True)
+model.fit_generator(train_generator, steps_per_epoch= len(train_data), validation_data = validate_generator,
+                        validation_steps=len(validation_data), epochs = 1)
+model.save(file)
+
+# def get_cnn_model():
+#     model = Sequential()
+#     model.add(Cropping2D(cropping = ((75,25),(0,0)),
+#                         input_shape= (160,320,3),
+#                         data_format = "channels_last"))
+    
+#     model.add(Lambda(lambda x: (x/127.5) - 0.5))
+
+#     model.add(Conv2D(3, (1, 1), padding='same'))
+#     model.add(ELU())
+
+#     model.add(BatchNormalization())
+#     model.add(Conv2D(16, (5, 5), strides= (2, 2), padding="same"))
+#     model.add(ELU())
+
+#     model.add(BatchNormalization())
+#     model.add(Conv2D(32, (5, 5), strides= (2, 2), padding="same"))
+#     model.add(ELU())
+
+#     model.add(BatchNormalization())
+#     model.add(Conv2D(64, (5, 5), strides= (2, 2), padding="same"))
+#     model.add(ELU())
+
+#     model.add(BatchNormalization())
+#     model.add(Conv2D(128, (5, 5), strides= (2, 2), padding="same"))
+#     model.add(ELU())
+
+#     model.add(Flatten())
+#     model.add(ELU())
+
+#     model.add(Dense(512))
+#     model.add(Dropout(.2))
+#     model.add(ELU())
+
+#     model.add(Dense(100))
+#     model.add(Dropout(.5))
+#     model.add(ELU())
+
+#     model.add(Dense(10))
+#     model.add(Dropout(.5))
+#     model.add(ELU())
+
+#     model.add(Dense(11))
+
+#     return model
+
+
+# model = get_cnn_model()
+# # model.compile(optimizer='adam', loss='mse')
+# model.compile(optimizer=Adam(lr = 1e-5), loss='mse', metrics=['accuracy'])
+
+# plot_model(model, '{}.png'.format("cnn"), show_shapes=True)
+# print(model.summary())
 
 # file = 'model_gen.h5'
-# earlystopper = EarlyStopping(patience=5, verbose = 1)
-# checkpointer = ModelCheckpoint(file, monitor='val_loss', verbose = 1, save_best_only=True)
-# model.fit_generator(train_generator, steps_per_epoch= len(train_data), validation_data = validate_generator,
-#                         validation_steps=len(validation_data), epochs = 1)
-# model.save(file)
+# callbacks = [
+#         # Save best model
+#         ModelCheckpoint(file, monitor='val_loss', verbose = 1, save_best_only=True),
+#         # Stop training after 5 epochs without improvement
+#         EarlyStopping(patience=5, verbose=1)
+#     ]
+
+# history_object = model.fit_generator(
+#         train_generator,
+#         steps_per_epoch=len(train_data),
+#         epochs=1,
+#         validation_data=validate_generator,
+#         validation_steps=len(validation_data)
+#     )
+
+# print(history_object.history.keys())
+
+# ### plot the training and validation loss for each epoch
+# plt.plot(history_object.history['loss'])
+# plt.plot(history_object.history['val_loss'])
+# plt.title('model mean squared error loss')
+# plt.ylabel('mean squared error loss')
+# plt.xlabel('epoch')
+# plt.legend(['training set', 'validation set'], loc='upper right')
+# plt.show()
